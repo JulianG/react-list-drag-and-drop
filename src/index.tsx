6655af019a2dd9a9e313efd20afacb923c48760b
JulianG <julian.garamendy@king.com>
2018-04-29 08:21:30 +0100

    adding why-did-you-update

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,8 @@ import { render } from 'react-dom';
 import Example from './examples/Example';
 
 render(<Example />, document.getElementById('root'));
+
+if (process.env.NODE_ENV !== 'production') {
+  const { whyDidYouUpdate } = require('why-did-you-update');
+  whyDidYouUpdate(React);
+}
