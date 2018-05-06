3a11998da014cebdadff8767799351a816596539
JulianG <juliangaramendy@gmail.com>
2018-05-06 22:53:39 +0100

    re-enabling some tests

--- src/tests/GGeometry.test.ts
+++ src/tests/GGeometry.test.ts
@@ -115,32 +115,32 @@ test('Two overlapping rects, when r0 is NW os r1, have the expected overlapping 
 	expect(Geom.getAreaOfIntersection(r0, r1)).toBe(25);
 });
 
-_test('Two rects are overlapping, when r0 is NE of r1, have the expected overlapping area', () => {
+test('Two rects are overlapping, when r0 is NE of r1, have the expected overlapping area', () => {
 	const r0: Geom.Rect = { top: 5, left: 15, width: 10, height: 10 };
 	const r1: Geom.Rect = { top: 10, left: 10, width: 10, height: 10 };
 	expect(Geom.getAreaOfIntersection(r0, r1)).toBe(25);
 });
 
-_test('Two Rects are overlapping, when r0 is SE of r1, have the expected overlapping area', () => {
+test('Two Rects are overlapping, when r0 is SE of r1, have the expected overlapping area', () => {
 	const r0: Geom.Rect = { top: 15, left: 15, width: 10, height: 10 };
 	const r1: Geom.Rect = { top: 10, left: 10, width: 10, height: 10 };
 	expect(Geom.getAreaOfIntersection(r0, r1)).toBe(25);
 });
 
-_test('Two Rects are overlapping, when r0 is SW of r1, have the expected overlapping area', () => {
+test('Two Rects are overlapping, when r0 is SW of r1, have the expected overlapping area', () => {
 	const r0: Geom.Rect = { top: 15, left: 5, width: 10, height: 10 };
 	const r1: Geom.Rect = { top: 10, left: 10, width: 10, height: 10 };
 	expect(Geom.getAreaOfIntersection(r0, r1)).toBe(25);
 });
 
-_test('Two Rects are overlapping, when r0 is within r1, have the expected overlapping area', () => {
+test('Two Rects are overlapping, when r0 is within r1, have the expected overlapping area', () => {
 	const r0: Geom.Rect = { top: 12, left: 12, width: 6, height: 6 };
 	const r1: Geom.Rect = { top: 10, left: 10, width: 10, height: 10 };
-	expect(Geom.getAreaOfIntersection(r0, r1)).toBe(25);
+	expect(Geom.getAreaOfIntersection(r0, r1)).toBe(36);
 });
 
-_test('Two Rects are overlapping, when r1 is within r0, have the expected overlapping area', () => {
+test('Two Rects are overlapping, when r1 is within r0, have the expected overlapping area', () => {
 	const r0: Geom.Rect = { top: 10, left: 10, width: 10, height: 10 };
 	const r1: Geom.Rect = { top: 12, left: 12, width: 6, height: 6 };
-	expect(Geom.getAreaOfIntersection(r0, r1)).toBe(25);
+	expect(Geom.getAreaOfIntersection(r0, r1)).toBe(36);
 });
